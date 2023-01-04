2a851c4bbbd32c93628ede062242d7b0005f585e
Artyom Antyipin <artyom.szasa@gmail.com>
2023-01-04 08:00:55 +0100

    further refactor, array-of fix in portable context.

--- NCoreUtils.Data.Protocol.Client.Common/CommonClientFunctions/ArrayOf.cs
+++ NCoreUtils.Data.Protocol.Client.Common/CommonClientFunctions/ArrayOf.cs
@@ -9,27 +9,29 @@ public class ArrayOf : IFunctionMatcher
 {
     public static FunctionMatch MatchEnumerableOrArray(IDataUtils utils, Expression expression)
     {
-        if (utils.IsArray(expression.Type) && expression.TryExtractConstant(out var boxed) && boxed is Array array)
+        if (expression.TryExtractConstant(out var boxed))
         {
-            var length = array.GetLength(0);
-            var arrayExpression = Expression.Constant(array, expression.Type);
-            var arguments = new Expression[length];
-            for (var i = 0; i < arguments.Length; ++i)
+            if (utils.IsArray(expression.Type) && boxed is Array array)
             {
-                arguments[i] = Expression.ArrayIndex(arrayExpression, Expression.Constant(i, typeof(int)));
+                var length = array.GetLength(0);
+                var arrayExpression = Expression.Constant(array, expression.Type);
+                var arguments = new Expression[length];
+                for (var i = 0; i < arguments.Length; ++i)
+                {
+                    arguments[i] = Expression.ArrayIndex(arrayExpression, Expression.Constant(i, typeof(int)));
+                }
+                return new(Names.Array, arguments);
             }
-            return new(Names.Array, arguments);
-        }
-        if (utils.IsEnumerable(expression.Type, out var elementType)
-            && expression.TryExtractConstant(out boxed)
-            && boxed is System.Collections.IEnumerable enumerable)
-        {
-            var arguments = new List<Expression>();
-            foreach (var item in enumerable)
+            if (utils.IsEnumerable(expression.Type, out var elementType)
+                && boxed is System.Collections.IEnumerable enumerable)
             {
-                arguments.Add(utils.CreateBoxedConstant(elementType, item));
+                var arguments = new List<Expression>();
+                foreach (var item in enumerable)
+                {
+                    arguments.Add(utils.CreateBoxedConstant(elementType, item));
+                }
+                return new(Names.Array, arguments);
             }
-            return new(Names.Array, arguments);
         }
         return default;
     }
